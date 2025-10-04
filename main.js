0e84ecd57e159300044741e0e8da075af384fe34
mohamed azoz <125761608+MohamedAzoz@users.noreply.github.com>
2025-10-04 19:18:48 +0300

    update

--- main.js
+++ main.js
@@ -1,40 +1,13 @@
-// "use strict";
+"use strict";
 
-// let btn=document.getElementById("btn");
-
-
-// btn.addEventListener('click',
-// function () {
-//     const domain=document.getElementById("ipInput").value;
-//     console.log(domain);
-    
-//     fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_M1nEtlqNLx7SYHQ2ahehO5YpCMstx&domain=${domain}`,{
-//         method:"GET"
-//         })
-//         .then(res=>res.json())
-//         .then(data=>{
-//         // console.log(data);
-//         // console.log(data.location.lat);
-
-//         const map = new L.Map('map').setView([data.location.lat,data.location.lng], 13);
-
-//         const tiles = new L.TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//             maxZoom: 18,
-//             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//         }).addTo(map);
-//      });
-// });
-
-
-// افتراض: لديك map و marker مُنشئين مسبقًا (كما فعلت قبل)
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([29.98333, 31.21667], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-const marker = L.marker([51.505, -0.09]).addTo(map);
+const marker = L.marker([29.98333 ,31.21667]).addTo(map);
 
 
 async function geocodeAndMove(placeName) {
  
-  const q = (placeName.trim())?placeName.trim():"192.212.174.101";
+  const q = (placeName.trim())?placeName.trim():"197.38.171.88";
   const url = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_M1nEtlqNLx7SYHQ2ahehO5YpCMstx&domain=${q}`;
 
   try {
@@ -48,9 +21,9 @@ async function geocodeAndMove(placeName) {
       return;
     }
 
-    const lat = parseFloat(results.location.lat);
-    const lng = parseFloat(results.location.lng);
-    console.log(results);
+    // const lat = parseFloat(results.location.lat);
+    // const lng = parseFloat(results.location.lng);
+    // console.log(results);
     // حدّث الخريطة و الماركر
     map.setView([lat, lng], 13);
     marker.setLatLng([lat, lng]);
@@ -59,9 +32,9 @@ async function geocodeAndMove(placeName) {
     document.getElementById("Timezone").innerHTML=`UTC ${results.location.timezone}`;
     document.getElementById("ISP").innerHTML=`${results.isp}`;
     
-    console.log("Moved to:", lat, lng, results);
+    // console.log("Moved to:", lat, lng, results);
   } catch (err) {
-    console.error("Geocoding error:", err);
+    // console.error("Geocoding error:", err);
     // alert("حدث خطأ أثناء البحث عن الموقع.");
   }
 }
